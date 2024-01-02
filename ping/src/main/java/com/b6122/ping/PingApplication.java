1b46f271a610d3569829be7ec868ae838bfda804
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-01-02 15:21:46 +0900

    프로젝트 환경설정

---
+++ ping/src/main/java/com/b6122/ping/PingApplication.java
@@ -0,0 +1,13 @@
+package com.b6122.ping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingApplication.class, args);
+	}
+
+}
