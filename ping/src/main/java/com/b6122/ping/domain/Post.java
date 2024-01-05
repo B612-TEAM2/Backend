3afebc756c183f2bc85a66d24cdb028b0cba3be9
RyuJiye <coomicuma@naver.com>
2024-01-05 13:00:13 +0900

    Post

--- ping/src/main/java/com/b6122/ping/domain/Post.java
+++ ping/src/main/java/com/b6122/ping/domain/Post.java
@@ -1,22 +1,33 @@
 package com.b6122.ping.domain;
 
-import com.fasterxml.jackson.databind.util.ArrayBuilders;
 import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+
 
 @Entity
 @Table(name = "post")
 public class Post {
-    @Id @GeneratedValue
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "post_id")
     private  long id; //post id
-
+    @Column(name = "title")
     private char title; // 제목
 
+    @ColumnDefault("0")
+    @Column(name = "likeNum")
     private int likeNum; //좋아요 수
 
+    @ColumnDefault("0")
+    @Column(name = "viewerNum")
     private int viewerNum; // 조회수
 
-    //content, date 추가 구현 필요
+
+    @Column(name = "content", nullable = false)
+    private String content;
 
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -24,7 +35,7 @@ public class Post {
     private User user; //사용자
 
     @Enumerated(EnumType.STRING)
-    private PostAccess access; //공개 범위 [private, friend, public]
+    private PostScope scope; //공개 범위 [private, friends, public]
 
     //연관관계 매서드//
     public void setUser(User user) {
