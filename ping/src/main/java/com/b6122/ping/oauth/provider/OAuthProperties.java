7d8470c8a6e6f012fecc127eecfb30fb98b7a85a
RyuJiye <coomicuma@naver.com>
2024-01-19 16:45:03 +0900

    Naver 소셜로그인 구현

--- ping/src/main/java/com/b6122/ping/oauth/provider/OAuthProperties.java
+++ ping/src/main/java/com/b6122/ping/oauth/provider/OAuthProperties.java
@@ -2,40 +2,14 @@ package com.b6122.ping.oauth.provider;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.security.SecureRandom;
+import java.math.BigInteger;
 @Component
 public class OAuthProperties {
 
-    // Kakao OAuth properties
-    @Value("${kakao.client-id}")
-    public static String KAKAO_CLIENT_ID;
+    public static String NAVER_CLIENT_ID = "HT4vaEYy1mRjQXVadpwE";
+    public static String NAVER_CLIENT_SECRET = "cDBX96chK3";
 
-    @Value("${kakao.client-secret}")
-    public static String KAKAO_CLIENT_SECRET;
+    public static String NAVER_STATE = new BigInteger(130, new SecureRandom()).toString(32);
 
-    @Value("${kakao.redirect-uri}")
-    public static String KAKAO_REDIRECT_URI;
-
-    // Google OAuth properties
-    @Value("${google.client-id}")
-    public static String GOOGLE_CLIENT_ID;
-
-    @Value("${google.client-secret}")
-    public static String GOOGLE_CLIENT_SECRET;
-
-    @Value("${google.redirect-uri}")
-    public static String GOOGLE_REDIRECT_URI;
-
-    // Naver OAuth properties (if applicable)
-    @Value("${naver.client-id}")
-    public static String NAVER_CLIENT_ID;
-
-    @Value("${naver.client-secret}")
-    public static String NAVER_CLIENT_SECRET;
-
-    @Value("${naver.redirect-uri}")
-    public static String NAVER_REDIRECT_URI;
-
-    // Add any other OAuth provider properties as needed
-
-    // Constructors, getters, and setters can be added if required
 }
