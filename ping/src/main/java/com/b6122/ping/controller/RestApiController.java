ad34a29575c23fba572c955bc84087f7d42684cb
RyuJiye <coomicuma@naver.com>
2024-01-19 13:44:05 +0900

    Post 수정, google 로그인 구현

--- ping/src/main/java/com/b6122/ping/controller/RestApiController.java
+++ ping/src/main/java/com/b6122/ping/controller/RestApiController.java
@@ -6,6 +6,7 @@ import com.b6122.ping.dto.CreateJwtRequestDto;
 import com.b6122.ping.dto.UserDto;
 import com.b6122.ping.service.JwtService;
 import com.b6122.ping.service.UserService;
+import com.b6122.ping.service.GoogleOAuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
@@ -25,11 +26,21 @@ public class RestApiController {
     private final UserService userService;
 
     @PostMapping("/oauth/jwt/google")
-    public ResponseEntity<Map<String, String>> createJwt(@RequestBody CreateJwtRequestDto jwtRequestDto) {
-        //회원가입
-        UserDto userDto = userService.joinOAuthUser(jwtRequestDto);
+    public ResponseEntity<Map<String, String>> createGoogleJwt(@RequestBody Map<String, Object> request) throws IOException {
+        // Frontend sends the authorization code to the server
+        String authorizationCode = request.get("code").toString();
 
-        //jwt accessToken 발급
+        // Exchange the authorization code for an access token from Google
+        String accessToken = GoogleOAuthService.getGoogleAccessToken(authorizationCode);
+
+        // Use the access token to fetch user information from Google
+        Map<String, Object> userInfo = GoogleOAuthService.getGoogleUserInfo(accessToken);
+
+        // Process the user information and perform user registration if needed
+        UserDto userDto = userService.joinOAuthUser(userInfo);
+
+        // Return the JWT access token to the React server
         return ResponseEntity.ok().body(jwtService.createJwtAccessToken(userDto));
     }
+
 }
