6a0f46a17679092493d7925e93becc8e60d834f3
RyuJiye <coomicuma@naver.com>
2024-01-23 18:44:53 +0900

    Create OAuthProperties.java

---
+++ ping/src/main/java/com/b6122/ping/oauth/provider/OAuthProperties.java
@@ -0,0 +1,25 @@
+package com.b6122.ping.oauth.provider;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.SecureRandom;
+import java.math.BigInteger;
+@Component
+public class OAuthProperties {
+
+
+
+    String KAKAO_CLIENT_ID = "4294c81106f19588526f3a34cb2b4356";
+    String KAKAO_REDIRECT_URI =  "http://localhost:3000/authkakao";
+
+    String GOOGLE_CLIENT_ID = "182133073202-g6sdd3ih0rpdlnjc14akqa1uj23ndbvh.apps.googleusercontent.com";
+    String GOOGLE_REDIRECT_URI = "http://localhost:3000/authgoogle";
+    String GOOGLE_CLIENT_SECRET = "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";
+
+
+    String NAVER_CLIENT_ID = "HT4vaEYy1mRjQXVadpwE";
+    String NAVER_CLIENT_SECRET = "cDBX96chK3";
+    String NAVER_REDIRECT_URI = "http://localhost:3000/authnaver";
+    String NAVER_STATE = new BigInteger(130, new SecureRandom()).toString(32);
+
+}
