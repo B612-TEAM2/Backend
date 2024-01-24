cd94bea82261072a032376bcd075c9dcfcfa2d45
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-01-24 22:34:37 +0900

    fix 친구 삭제 로직 변경

--- ping/src/main/java/com/b6122/ping/repository/datajpa/FriendshipDataRepository.java
+++ ping/src/main/java/com/b6122/ping/repository/datajpa/FriendshipDataRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 public interface FriendshipDataRepository extends JpaRepository<Friendship, Long> {
 
-    //친구 목록
+    //친구 목록 조회
     @Query("select f from Friendship f" +
             " join fetch f.fromUser" +
             " join fetch f.toUser" +
@@ -20,14 +20,14 @@ public interface FriendshipDataRepository extends JpaRepository<Friendship, Long
     //친구 삭제
     @Query("delete from Friendship f" +
             " where f.isFriend = true and" +
-            " ((f.fromUser.id =:userId and f.toUser.nickname =:nickname)" +
-            " or (f.toUser.id =:userId and f.fromUser.nickname =:nickname))")
-    void deleteFriendshipByFriendNicknameAndUserId(@Param("nickname") String friendNickname,
+            " ((f.fromUser.id =:userId and f.toUser.nickname =:friendId)" +
+            " or (f.toUser.id =:userId and f.fromUser.nickname =:friendId))")
+    void deleteFriendshipByIds(@Param("friendId") Long friendId,
                                                    @Param("userId") Long userId);
 
 
     /**
-     * FriendShip 엔티티 가져오기(fromUser가 사용자이고 isFriend가 true 일 때)
+     * 친구 단건 조회(fromUser가 사용자이고 isFriend가 true 일 때)
      */
     @Query("select f from Friendship f" +
             " where f.fromUser.id = :userId" +
