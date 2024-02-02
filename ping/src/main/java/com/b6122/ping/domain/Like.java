13b29bb978d3eaf8f685097265623047d1aa40c5
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-02 17:53:24 +0900

    fix error

--- ping/src/main/java/com/b6122/ping/domain/Like.java
+++ ping/src/main/java/com/b6122/ping/domain/Like.java
@@ -16,9 +16,9 @@ public class Like {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
-    private Long uid;
+    private User user;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "post_id")
-    private Long pid;
+    private Post post;
 }
