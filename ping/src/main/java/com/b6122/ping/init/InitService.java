fc6f799b02691c54c48c2948fc02c525520a7b10
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-06 00:23:20 +0900

    refactor UserService

--- ping/src/main/java/com/b6122/ping/init/InitService.java
+++ ping/src/main/java/com/b6122/ping/init/InitService.java
@@ -1,6 +1,7 @@
 package com.b6122.ping.init;
 
 import com.b6122.ping.domain.User;
+import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityManager;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -20,13 +21,16 @@ public class InitService {
     private String profileImagePath;
 
     public void dbInit() {
-        User user1 = User.createUser("nickname1",profileImagePath + "\\" + "dummy1.png");
+        User user1 = User.createUser();
+        user1.updateDummyProfile("nickname1",profileImagePath + "\\" + "dummy1.png");
         em.persist(user1);
 
-        User user2 = User.createUser("nickname2",profileImagePath + "\\" + "dummy2.png");
+        User user2 = User.createUser();
+        user2.updateDummyProfile("nickname2",profileImagePath + "\\" + "dummy2.png");
         em.persist(user2);
 
-        User user3 = User.createUser("nickname3", profileImagePath + "\\" + "dummy3.png");
+        User user3 = User.createUser();
+        user3.updateDummyProfile("nickname3",profileImagePath + "\\" + "dummy3.png");
         em.persist(user3);
     }
 }
