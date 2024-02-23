7461abf38ca2600c9a185b5b93df69e3b716aa1a
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-23 17:56:36 +0900

    fixed untracked files

--- ping/src/main/java/com/b6122/ping/NcpObjectStorageConfig.java
+++
@@ -1,8 +0,0 @@
-package com.b6122.ping;
-
-public interface NcpObjectStorageConfig {
-    String accessKey = "accesskey";
-    String secretKey = "secretkey";
-    String ProfileImgBucketName = "ping/profile";
-    String endPoint = "https://kr.object.ncloudstorage.com";
-}
