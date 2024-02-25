ac585145e093810dafd6c41a85fca5424229b145
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-25 13:52:57 +0900

    fix 글 수정 컨트롤러 수정

--- ping/src/main/java/com/b6122/ping/service/PostService.java
+++ ping/src/main/java/com/b6122/ping/service/PostService.java
@@ -13,6 +13,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -44,7 +45,6 @@ public class PostService {
         post.setViewCount(postDto.getViewCount());
         post.setLikeCount(postDto.getLikeCount());
         post.setLikes(postDto.getLikes());
-//        post.saveImagesInStorage(postDto.getImgs()); //이미지 저장 MultiPartfile->path
         if(postDto.getImgs() != null) {
             //이미지 저장 MultiPartfile->path
 
@@ -57,9 +57,10 @@ public class PostService {
     }
 
     //post 수정
+    @Transactional
     public Long modifyPost(PostDto postDto) {
-        Post post;
-        post = new Post();
+
+        Post post = postRepository.findById(postDto.getId());
         post.setId(postDto.getId());
         post.setLocation(postDto.getLocation());
         post.setLatitude(postDto.getLatitude());
@@ -67,8 +68,14 @@ public class PostService {
         post.setTitle(postDto.getTitle());
         post.setContent(postDto.getContent());
         post.setScope(postDto.getScope());
-        post.saveImagesInStorage(postDto.getImgs()); //이미지 저장 MultiPartfile->path
-        return postRepository.updatePost(post);
+        if(postDto.getImgs() != null) {
+            //이미지 저장 MultiPartfile->path
+            List<String> paths = post.saveImagesInStorage(postDto.getImgs());
+            for (String path : paths) {
+                post.addImgPath(path);
+            }
+        }
+        return post.getId();
     }
 
 
@@ -92,8 +99,8 @@ public class PostService {
     //for문 마다 확인하기엔 통신이 너무 오래 걸림, db에서 한번에 검사하는게 효율적
     //postid, MyLike 받아서 체크
     public void toggleLike(List<Long> pids, List<Boolean> myLikes, Long uid) {
-        List<Long> delLikePids = null;
-        List<Long> insertLikePids = null;
+        List<Long> delLikePids = new ArrayList<>();
+        List<Long> insertLikePids = new ArrayList<>();
         for (int i = 0; i < pids.size(); i++) {
             if (!myLikes.get(i)) {
                 delLikePids.add(pids.get(i));
@@ -107,7 +114,7 @@ public class PostService {
 
         //pin 클릭시 해당 위치의 postList 반환,home friend public 동일한 함수 사용
         public List<PostDto> getPostsPreviewPin(List<Long> pids) {
-            List<Post> posts = null;
+            List<Post> posts = new ArrayList<>();
             for (Long pid : pids) {
                 posts.add(postRepository.findById(pid));
             }
@@ -125,8 +132,7 @@ public class PostService {
         //Home-List 토글, postList 반환
         public List<PostDto> getPostsHomeList (Long uid){
             List<Post> posts = postRepository.findByUid(uid);
-            System.out.println("posts = " + posts);
-            System.out.println("posts = " + posts);
+
             return posts.stream()
                     .map(post -> PostDto.postPreviewList(post, likeRepository))
                     .collect(Collectors.toList());
@@ -138,7 +144,7 @@ public class PostService {
     //Friends
     //친구가 작성한 글의 pin 반환
     public List<PostDto> getPinsFriendsMap(List<Long> uids) {
-        List<Post> posts = null;
+        List<Post> posts = new ArrayList<>();
         for(Long uid : uids){
              posts.addAll(postRepository.findNonePrivateByUid(uid));
         }
@@ -149,7 +155,7 @@ public class PostService {
 
         //친구 post list preview
         public List<PostDto> getPostsFriendsList (List < Long > uids) {
-            List<Post> posts = null;
+            List<Post> posts = new ArrayList<>();
             for (Long uid : uids) {
                 posts.addAll(postRepository.findNonePrivateByUid(uid));
             }
