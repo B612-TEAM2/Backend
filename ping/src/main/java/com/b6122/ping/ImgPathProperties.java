10fcdecc2684ebcf2d553b2aba445358438bd730
Hwan Jun Kim <spdlqjdkdlel1702@naver.com>
2024-02-28 15:34:40 +0900

    commit

--- ping/src/main/java/com/b6122/ping/ImgPathProperties.java
+++
@@ -1,6 +0,0 @@
-package com.b6122.ping;
-
-public interface ImgPathProperties {
-
-    String postImgPath = "C:\\ping\\post\\image";
-}
--- ping/src/main/java/com/b6122/ping/domain/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.b6122.ping.domain;
-
-public enum UserRole {
-    ROLE_USER,
-    ROLE_ADMIN
-}
